e13ff5e1245b089be0ec6fa93c1ba3e95a3dc6a0
Arslan <140950959+Arslanasif9t9@users.noreply.github.com>
2023-10-11 22:04:13 +0500

    redesign with table html 1

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import './App.css';
 import layout_img from './img/layout -1.svg'
 import countryFlag from './img/kuwait flag 1.svg'
+import Background from './img/Background 1.svg'
+import side from './img/Picture2 1.svg'
+import btn from './img/Rectangle 2.svg'
 
 function App() {
   var gradient = 'linear-gradient(91deg, #4D1E4C -0.89%, #902B78 68.07%, #A4276C 102.55%)';
-
+  var color = '#FF365D';
+  
   return (
-    <div style={{padding: '2rem', background: 'rgb(248 242 242)'}}>
-      <div className="bg-white p-3 m-auto" style={{width: '430px'}}>
-        <img src={layout_img} className='m-auto pt-4'/>
-        <button className='text-white text-2xl font-semibold w-11/12 py-1.5 px-4 mt-1 block m-auto rounded-full' style={{background: gradient}}>Service Name</button>
-        <div className='text-xs font-semibold border border-solid border-black rounded-xl px-4 block w-fit m-auto my-4'>Fills 750 for 1 week</div>
-        <p className='text-sm font-semibold text-center mt-8 mb-3'>Please enter your stc Mobile Number :</p>
-        <div className="flex justify-center justify-items-center px-5 py-2.5 border border-solid border-black rounded-full m-auto w-fit">
-          <div className="flex justify-center justify-items-center">
-            <img src={countryFlag} />
-            <span className='text-xl font-bold pl-1 pr-2 border-r border-solid border-black'>+965</span>
-          </div>
-          <input className='w-36 pl-3 text-xl font-bold center' type="text" value="300-555-0399" />
-        </div>
-        <button className='text-white text-2xl font-bold w-11/12 py-3 px-4 my-9 block m-auto rounded-full' style={{background: gradient}}>SUBSCRIBE</button>
-        <div>
-          <h4 className='text-xl py-2 font-bold text-center'>Terms & Conditions</h4>
-          <p className='text-sm px-6 font-semibold text-center '>You will be subscribed to “Service Name”. Your account would be debited with Fills 750 for 1 week(s). Subscription would be automatically renewed until you unsubscribe from the service. To continue, click on the Subscribe button above. Please make sure that your browser is not using any 3rd party blocking technologies and you have a healthy internet connection for swift access to content.</p>
-        </div>
-        <div className='flex flex-wrap gap-3 justify-around mt-7 mb-2'>
-          <button className='px-5 py-1.5 border border-solid border-black rounded-full text-sm font-bold text-white' style={{background: gradient}}>Home</button>
-          <button className='px-5 py-1.5 border border-solid border-black rounded-full text-sm font-bold'>Terms</button>
-          <button className='px-5 py-1.5 border border-solid border-black rounded-full text-sm font-bold'>Manage</button>
-          <button className='px-5 py-1.5 border border-solid border-black rounded-full text-sm font-bold'>Contact</button>
-        </div>
-        <footer className='text-sm font-semibold text-center'>© All rights reserved 2023</footer>
-      </div>
+    <div style={{padding: '2rem', background: 'rgb(232 232 232)'}}>
+
+      <table style={{width: '200px', height: '520px', boxSizing: 'border-box', padding: '36px 16px', overflow: 'hidden', background: 'white'}}>
+        <tbody style={{display: 'block'}}>
+          <tr style={{display: 'block'}}>
+            <td style={{display: 'block'}}><img style={{width: '90%', margin: '10px auto 1.5px'}} src={layout_img} /></td>
+          </tr>
+          <tr style={{display: 'block'}}>
+            <td style={{display: 'block'}}><button style={{color: 'white', background: gradient, fontSize: '12px', fontWeight: '600', width: '90%',display: 'block', margin: 'auto', borderRadius: '100px', padding: '3px'}}>Service Name</button></td>
+          </tr>
+          <tr style={{display: 'block'}}>
+            <td style={{display: 'block'}}><div style={{fontSize: '7px', fontWeight: '400', border: '1px solid black', borderRadius: '100px', padding: '0 5px', width: '40%', margin: '5px auto', textAlign: 'center'}}>Fills 750 for 1 week</div></td>
+          </tr>
+          <tr style={{display: 'block'}}>
+            <td style={{display: 'block'}}><p style={{fontSize: '7px', fontWeight: '500', textAlign: 'center', marginTop: '9px'}}>Please enter your stc Mobile Number :</p></td>
+          </tr>
+          <tr style={{display: 'block'}}>
+            <td style={{display: 'block', width: '70%',height: '100%', margin: '5px auto', border: '1px solid black', borderRadius: '100px', padding: '0px 8px 4px'}}>
+              <div>
+                <img style={{display: 'inline', width: '13px'}} src={countryFlag} />
+                <span style={{display: 'inline-block', fontSize: '10px', fontWeight: '600', padding: '0 4px 0 2px', borderRight: '1px solid black'}}>+965</span>
+                <input style={{display: 'inline', width: '70px', fontSize: '10px', fontWeight: '600', paddingLeft: '7px'}} type="text" value="300-555-0399" />
+              </div>
+            </td>
+          </tr>
+          <tr style={{display: 'block'}}>
+            <td style={{display: 'block'}}><button style={{background: gradient, color: 'white', fontSize: '14px', fontWeight: '600', display: 'block', width: '90%', margin: '10px auto', borderRadius: '100px', padding: '4px 0'}}>SUBSCRIBE</button></td>
+          </tr>
+          <tr style={{display: 'block', margin: '8px 12px 7px'}}>
+            <td style={{display: 'block'}}><h4 style={{fontSize: '11px', fontWeight: '700', textAlign: 'center'}}>Terms & Conditions</h4></td>
+            <td style={{display: 'block'}}><p style={{fontSize: '7px', fontWeight: '500', textAlign: 'center'}}>You will be subscribed to “Service Name”. Your account would be debited with Fills 750 for 1 week(s). Subscription would be automatically renewed until you unsubscribe from the service. To continue, click on the Subscribe button above. Please make sure that your browser is not using any 3rd party blocking technologies and you have a healthy internet connection for swift access to content.</p></td>
+          </tr>
+          <tr style={{display: 'block', width: '97%', margin: 'auto', boxSizing: 'border-box'}}>
+            <td style={{display: 'inline-block', width: '25%'}}><button style={{display: 'block', width: '38px', fontSize: '6px', fontWeight: '500', color: 'white', padding: '2px 0', borderRadius: '100px', margin: 'auto', border: '1px solid black', background: gradient}}>Home</button></td>
+            <td style={{display: 'inline-block', width: '25%'}}><button style={{display: 'block', width: '38px', fontSize: '6px', fontWeight: '500', color: 'black', padding: '2px 0', borderRadius: '100px', margin: 'auto', border: '1px solid black'}}>Terms</button></td>
+            <td style={{display: 'inline-block', width: '25%'}}><button style={{display: 'block', width: '38px', fontSize: '6px', fontWeight: '500', color: 'black', padding: '2px 0', borderRadius: '100px', margin: 'auto', border: '1px solid black'}}>Manage</button></td>
+            <td style={{display: 'inline-block', width: '25%'}}><button style={{display: 'block', width: '38px', fontSize: '6px', fontWeight: '500', color: 'black', padding: '2px 0', borderRadius: '100px', margin: 'auto', border: '1px solid black'}}>Contact</button></td>
+          </tr>
+          <tr style={{display: 'block'}}>
+            <td style={{display: 'block'}}><footer style={{fontSize: '6px', textAlign: 'center', margin: '4px 0'}}>© All rights reserved 2023</footer></td>
+          </tr>
+        </tbody>
+      </table>
+
     </div>
   );
 }
